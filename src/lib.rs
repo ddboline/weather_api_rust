eff019ea41efd827bf7b01f1ef10f28013dd7767
Daniel Boline <ddboline@gmail.com>
2020-01-31 16:19:59 -0500

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::too_many_lines)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::cast_possible_truncation)]
+
 pub mod app;
 pub mod config;
 pub mod errors;
