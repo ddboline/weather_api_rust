3bc366af61cde1390d5d61b79c7c11878aa30494
Daniel Boline <ddboline@gmail.com>
2022-02-28 07:27:35 -0500

    desktop weather app

---
+++ src/weatherapp.rs
@@ -0,0 +1,319 @@
+use dioxus::prelude::*;
+use anyhow::{format_err, Error};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
+use stack_string::{StackString, format_sstr};
+use arc_swap::ArcSwap;
+use lazy_static::lazy_static;
+use im::HashMap;
+use std::sync::Arc;
+use chrono::FixedOffset;
+use std::fmt::Write;
+
+use weather_util_rust::weather_data::WeatherData;
+use weather_util_rust::weather_forecast::WeatherForecast;
+use weather_util_rust::weather_api::WeatherLocation;
+use weather_util_rust::config::Config;
+use weather_util_rust::weather_api::WeatherApi;
+
+lazy_static! {
+    pub static ref WEATHER_CACHE: ArcSwap<HashMap<StackString, (Option<WeatherData>, Option<WeatherForecast>)>> = ArcSwap::new(Arc::new(HashMap::new()));
+}
+
+static DEFAULT_STR: &'static str = "11106";
+
+fn main() -> Result<(), Error> {
+    let (send, mut recv) = unbounded_channel::<StackString>();
+    let config = Config::init_config()?;
+    let api_key = config.api_key.as_ref().ok_or_else(|| format_err!("No api key given"))?;
+    let api = WeatherApi::new(api_key.as_str(), &config.api_endpoint, &config.api_path);
+
+    let handle: std::thread::JoinHandle<Result<(), Error>> = std::thread::spawn(move || {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()?
+            .block_on(
+                async move {
+                    while let Some(msg) = recv.recv().await {
+                        println!("grab {msg} weather");
+                        let loc = get_parameters(&msg);
+                        let weather = api.get_weather_data(&loc).await.ok();
+                        let forecast = api.get_weather_forecast(&loc).await.ok();
+                        let new_cache = Arc::new(WEATHER_CACHE.load().update(msg, (weather, forecast)));
+                        WEATHER_CACHE.store(new_cache);
+                    }
+                }
+            );
+        Ok(())
+    });
+
+    send.send(DEFAULT_STR.into())?;
+    loop {
+        if WEATHER_CACHE.load().contains_key(DEFAULT_STR) {
+            break;
+        }
+        std::thread::sleep(std::time::Duration::from_millis(10));
+    }
+
+    dioxus::desktop::launch_with_props(
+        app,
+        AppProps {
+                send,
+            },
+            |c| c,
+    );
+    handle.join().unwrap()?;
+    Ok(())
+}
+
+struct AppProps {
+    send: UnboundedSender<StackString>,
+}
+
+fn app(cx: Scope<AppProps>) -> Element {
+    let (search_str, set_search_str) = use_state(&cx, || StackString::new());
+    let (weather_default, forecast_default) = {
+        let weather_cache = WEATHER_CACHE.load().clone();
+        let (weather, forecast) = weather_cache.get(DEFAULT_STR).unwrap();
+        (weather.as_ref().unwrap().clone(), forecast.as_ref().unwrap().clone())
+    };
+    let (weather, set_weather) = use_state(&cx, || weather_default);
+    let (forecast, set_forecast) = use_state(&cx, || forecast_default);
+    let (draft, set_draft) = use_state(&cx, || search_str.to_owned());
+
+    cx.render(rsx!(
+        link { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" },
+        div { class: "mx-auto p-4 bg-gray-100 h-screen flex justify-center",
+            div { class: "flex items-center justify-center flex-col",
+                div { 
+                    div { class: "inline-flex flex-col justify-center relative text-gray-500",
+                        div { class: "relative",
+                            input { class: "p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent",
+                                placeholder: "search...",
+                                "type": "text",
+                                value: "{draft}",
+                                oninput: move |evt| {
+                                    let msg = evt.value.as_str();
+                                    set_draft(evt.value.as_str().into());
+                                    if let Some((weather, forecast)) = WEATHER_CACHE.load().get(msg) {
+                                        if let Some(weather) = weather {
+                                            set_weather(weather.clone());
+                                        }
+                                        if let Some(forecast) = forecast {
+                                            set_forecast(forecast.clone());
+                                        }
+                                    }
+                                },
+                                onkeydown: move |evt| {
+                                    if let Some((weather, forecast)) = WEATHER_CACHE.load().get(draft) {
+                                        if let Some(weather) = weather {
+                                            set_weather(weather.clone());
+                                        }
+                                        if let Some(forecast) = forecast {
+                                            set_forecast(forecast.clone());
+                                        }
+                                    }
+                                    if evt.key == "Enter" {
+                                        set_search_str(draft.clone());
+                                        cx.props.send.send(draft.clone()).unwrap();
+                                        loop {
+                                            if let Some((weather, forecast)) = WEATHER_CACHE.load().get(draft) {
+                                                if let Some(weather) = weather {
+                                                    set_weather(weather.clone());
+                                                }
+                                                if let Some(forecast) = forecast {
+                                                    set_forecast(forecast.clone());
+                                                }
+                                                break;
+                                            }
+                                            std::thread::sleep(std::time::Duration::from_millis(10));
+                                        }
+                                    }
+                                },
+                            }
+                            svg { class: "w-4 h-4 absolute left-2.5 top-3.5",
+                                "viewBox": "0 0 24 24",
+                                fill: "none",
+                                stroke: "currentColor",
+                                xmlns: "http://www.w3.org/2000/svg",
+                                path { 
+                                    d: "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z",
+                                    "stroke-linejoin": "round",
+                                    "stroke-linecap": "round",
+                                    "stroke-width": "2",
+                                }
+                            }
+                        }
+                    }
+                }
+                div { class: "flex flex-wrap w-full px-2",
+                    div { class: "bg-gray-900 text-white relative min-w-0 break-words rounded-lg overflow-hidden shadow-sm mb-4 w-full bg-white dark:bg-gray-600",
+                        div { class: "px-6 py-6 relative",
+                            country_info( weather: &weather, forecast: &forecast )
+                            country_data( weather: &weather, forecast: &forecast )
+                        }
+                        week_weather( weather: &weather, forecast: &forecast )
+                    }
+                }
+            }
+        }
+    ))
+}
+
+#[derive(Props)]
+struct WeatherForecastProp<'a> {
+    weather: &'a WeatherData,
+    forecast: &'a WeatherForecast,
+}
+
+fn country_data<'a>(cx: Scope<'a, WeatherForecastProp<'a>>) -> Element {
+    let weather = cx.props.weather;
+    let temp = weather.main.temp.fahrenheit();
+    let feels = weather.main.feels_like.fahrenheit();
+    let min = weather.main.temp_min.fahrenheit();
+    let max = weather.main.temp_max.fahrenheit();
+
+    cx.render(rsx!(
+        div { class: "block sm:flex justify-between items-center flex-wrap",
+            div { class: "w-full sm:w-1/2",
+                div { class: "flex mb-2 justify-between items-center",
+                    span { "Temp" }
+                    small { class: "px-2 inline-block", "{temp:0.2}°F" }
+                }
+            }
+            div { class: "w-full sm:w-1/2",
+                div { class: "flex mb-2 justify-between items-center",
+                    span { "Feels like" }
+                    small { class: "px-2 inline-block", "{feels:0.2}°F" }
+                }
+            }
+            div { class: "w-full sm:w-1/2",
+                div { class: "flex mb-2 justify-between items-center",
+                    span { "Temp min" }
+                    small { class: "px-2 inline-block", "{min:0.2}°F" }
+                }
+            }
+            div { class: "w-full sm:w-1/2",
+                div { class: "flex mb-2 justify-between items-center",
+                    span { "Temp max" }
+                    small { class: "px-2 inline-block", "{max:0.2}°F" }
+                }
+            }
+        }
+    ))
+}
+
+fn country_info<'a>(cx: Scope<'a, WeatherForecastProp<'a>>) -> Element {
+    let weather = cx.props.weather;
+    let name = &weather.name;
+    let country = weather.sys.country.as_ref().map_or("", StackString::as_str);
+    let mut main = StackString::new();
+    let mut desc = StackString::new();
+    let mut icon = StackString::new();
+    if let Some(weather) = weather.weather.get(0) {
+        main.push_str(&weather.main);
+        desc.push_str(&weather.description);
+        icon.push_str(&weather.icon);
+    }
+    let temp = weather.main.temp.fahrenheit();
+    let fo: FixedOffset = weather.timezone.into();
+    let date = weather.dt.with_timezone(&fo);
+
+    cx.render(rsx!(
+        div { class: "flex mb-4 justify-between items-center",
+            div {
+                h5 { class: "mb-0 font-medium text-xl",
+                    "{name} {country}"
+                }
+                small {
+                    img { class: "block w-8 h-8",
+                        src: "http://openweathermap.org/img/wn/{icon}@2x.png",
+                    }        
+                }
+            }
+            div { class: "text-right",
+                h6 { class: "mb-0",
+                    "{date}"
+                }
+                h3 { class: "font-bold text-4xl mb-0",
+                    span {
+                        "{temp:0.1}°F"
+                    }
+                }
+            }
+        }
+    ))
+}
+
+fn week_weather<'a>(cx: Scope<'a, WeatherForecastProp<'a>>) -> Element {
+    let forecast = cx.props.forecast;
+    let high_low = forecast.get_high_low();
+    cx.render(rsx!(
+        div { class: "divider table mx-2 text-center bg-transparent whitespace-nowrap",
+            span { class: "inline-block px-3", small { "Forecast" } }
+        }
+        div { class: "px-6 py-6 relative",
+            div { class: "text-center justify-between items-center flex",
+                style: "flex-flow: initial;",
+                high_low.iter().map(|(d, (h, l, r, s, i))| {
+                    let weekday = StackString::from_display(d.format("%a"));
+                    let low = l.fahrenheit();
+                    let high = h.fahrenheit();
+                    let mut rain = StackString::new();
+                    let mut snow = StackString::new();
+                    if r.millimeters() > 0.0 {
+                        rain = format_sstr!("R {:0.1}\"", r.inches());
+                    }
+                    if s.millimeters() > 0.0 {
+                        snow = format_sstr!("S {:0.1}\"", s.inches());
+                    }
+                    let mut icon = StackString::new();
+                    if let Some(i) = i.iter().next() {
+                        icon.push_str(i);
+                    }
+        
+                    rsx!(div {
+                            class: "text-center mb-0 flex items-center justify-center flex-col",
+                            span { class: "block my-1",
+                                "{weekday}"
+                            }
+                            img { class: "block w-8 h-8",
+                                src: "http://openweathermap.org/img/wn/{icon}@2x.png",
+                            }
+                            span { class: "block my-1",
+                                "{low:0.1}F°"
+                            }
+                            span { class: "block my-1",
+                                "{high:0.1}F°"
+                            }
+                            span { class: "block my-1",
+                                "{rain}"
+                            }
+                            span { class: "block my-1",
+                                "{snow}"
+                            }
+                        }
+                    )
+                })
+            }
+        }
+    ))
+}
+
+fn get_parameters(search_str: &str) -> WeatherLocation {
+    let mut opts = WeatherLocation::from_city_name(search_str);
+    if let Ok(zip) = search_str.parse::<u64>() {
+        opts = WeatherLocation::from_zipcode(zip);
+    } else if search_str.contains(',') {
+        let mut iter = search_str.split(',');
+        if let Some(lat) = iter.next() {
+            if let Ok(lat) = lat.parse() {
+                if let Some(lon) = iter.next() {
+                    if let Ok(lon) = lon.parse() {
+                        opts = WeatherLocation::from_lat_lon(lat, lon);
+                    }
+                }
+            }
+        }
+    }
+    opts
+}
