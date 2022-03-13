aac78ccd8004eeec1520a9de1161249d526c2b2a
Daniel Boline <ddboline@gmail.com>
2022-03-12 23:40:33 -0500

    avoid panic

--- src/api_options.rs
+++ src/api_options.rs
@@ -39,10 +39,6 @@ impl ApiOptions {
             }
         } else if let Some(city_name) = &self.q {
             WeatherLocation::from_city_name(city_name)
-        } else if self.lat.is_some() && self.lon.is_some() {
-            let lat = self.lat.unwrap().into();
-            let lon = self.lon.unwrap().into();
-            WeatherLocation::from_lat_lon(lat, lon)
         } else if let Some(zipcode) = config.zipcode {
             if let Some(country_code) = &config.country_code {
                 WeatherLocation::from_zipcode_country_code(zipcode, *country_code)
@@ -51,11 +47,17 @@ impl ApiOptions {
             }
         } else if let Some(city_name) = &config.city_name {
             WeatherLocation::from_city_name(city_name)
-        } else if config.lat.is_some() && config.lon.is_some() {
-            let lat = config.lat.unwrap();
-            let lon = config.lon.unwrap();
-            WeatherLocation::from_lat_lon(lat, lon)
         } else {
+            if let Some(lat) = self.lat {
+                if let Some(lon) = self.lon {
+                    return Ok(WeatherLocation::from_lat_lon(lat.into(), lon.into()));
+                }
+            }
+            if let Some(lat) = config.lat {
+                if let Some(lon) = config.lon {
+                    return Ok(WeatherLocation::from_lat_lon(lat, lon));
+                }
+            }
             return Err(Error::BadRequest(
                 "\n\nERROR: You must specify at least one option".into(),
             ));
