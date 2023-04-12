e7f2f6b4133df75097a1a9ae4aee85f76a502c0e
Daniel Boline <ddboline@gmail.com>
2023-04-11 23:31:55 -0400

    import/export

--- src/parse_opts.rs
+++ src/parse_opts.rs
@@ -1,10 +1,26 @@
 use anyhow::Error;
 use clap::Parser;
+use futures::{future::try_join_all, TryStreamExt};
 use refinery::embed_migrations;
+use stack_string::StackString;
+use std::path::PathBuf;
+use time::{macros::format_description, Date};
+use tokio::{
+    fs::{read, File},
+    io::{stdin, stdout, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+};
 
-use crate::{app::start_app, config::Config, pgpool::PgPool};
+use rweb_helper::DateType;
+
+use crate::{app::start_app, config::Config, pgpool::PgPool, WeatherDataDB};
 
 embed_migrations!("migrations");
+
+fn parse_date_from_str(s: &str) -> Result<DateType, String> {
+    Date::parse(s, format_description!("[year]-[month]-[day]"))
+        .map(Into::into)
+        .map_err(|e| format!("{e}"))
+}
 
 #[derive(Parser, Debug)]
 pub enum ParseOpts {
@@ -12,6 +28,26 @@ pub enum ParseOpts {
     RunMigrations,
     /// Run daemon
     Daemon,
+    /// Import into history
+    Import {
+        #[clap(short, long)]
+        /// Input file (if missinge will read from stdin)
+        filepath: Option<PathBuf>,
+    },
+    /// Export history
+    Export {
+        #[clap(short, long)]
+        server: Option<StackString>,
+        #[clap(short='b', long, value_parser=parse_date_from_str)]
+        /// Start date
+        start_time: Option<DateType>,
+        #[clap(short, long, value_parser=parse_date_from_str)]
+        /// End date
+        end_time: Option<DateType>,
+        #[clap(short, long)]
+        /// Output file (if missinge will read from stdin)
+        filepath: Option<PathBuf>,
+    },
 }
 
 impl ParseOpts {
@@ -33,8 +69,56 @@ impl ParseOpts {
             Self::Daemon => {
                 tokio::spawn(async move { start_app().await }).await??;
             }
-        }
+            Self::Import { filepath } => {
+                let db_url = config.database_url.as_ref().unwrap();
+                let pool = PgPool::new(db_url);
 
+                let data = if let Some(filepath) = filepath {
+                    read(&filepath).await?
+                } else {
+                    let mut stdin = stdin();
+                    let mut buf = Vec::new();
+                    stdin.read_to_end(&mut buf).await?;
+                    buf
+                };
+                let history: Vec<WeatherDataDB> = serde_json::from_slice(&data)?;
+                let futures = history.into_iter().map(|entry| {
+                    let pool = pool.clone();
+                    async move { entry.insert(&pool).await.map_err(Into::<Error>::into) }
+                });
+                let results: Result<Vec<u64>, Error> = try_join_all(futures).await;
+                let written: u64 = results?.into_iter().sum();
+                println!("written {written}");
+            }
+            Self::Export {
+                server,
+                start_time,
+                end_time,
+                filepath,
+            } => {
+                let db_url = config.database_url.as_ref().unwrap();
+                let pool = PgPool::new(db_url);
+                let results: Vec<_> = WeatherDataDB::get_by_name_dates(
+                    &pool,
+                    None,
+                    server.as_ref().map(|s| s.as_str()),
+                    start_time.map(Into::into),
+                    end_time.map(Into::into),
+                )
+                .await?
+                .try_collect()
+                .await?;
+
+                let mut file: Box<dyn AsyncWrite + Unpin + Send + Sync> =
+                    if let Some(filepath) = filepath {
+                        Box::new(File::create(&filepath).await?)
+                    } else {
+                        Box::new(stdout())
+                    };
+
+                file.write_all(&serde_json::to_vec(&results)?).await?;
+            }
+        }
         Ok(())
     }
 }
