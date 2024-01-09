2bbc88cd85538a7fdb7cb3df92cd30106c2e30e9
Daniel Boline <ddboline@gmail.com>
2024-01-08 23:32:34 -0500

    rustfmt

--- weather_app_wasm/src/main.rs
+++ weather_app_wasm/src/main.rs
@@ -1,12 +1,7 @@
-use weather_api_common::weather_element::WeatherAppComponent;
-use weather_api_common::weather_element::AppProps;
+use weather_api_common::weather_element::{AppProps, WeatherAppComponent};
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    dioxus_web::launch_with_props(
-        WeatherAppComponent,
-        AppProps,
-        dioxus_web::Config::default()
-    );
+    dioxus_web::launch_with_props(WeatherAppComponent, AppProps, dioxus_web::Config::default());
 }
